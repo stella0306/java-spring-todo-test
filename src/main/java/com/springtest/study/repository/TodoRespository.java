6284790d2489fb7d49f7783ba52cd09fab6f4422
stella0306 <starstella22222@gmail.com>
2025-03-15 16:44:37 +0900

    feat:: 누락된 어노테이션(Repository) 추가

--- src/main/java/com/springtest/study/repository/TodoRespository.java
+++ src/main/java/com/springtest/study/repository/TodoRespository.java
@@ -2,6 +2,8 @@ package com.springtest.study.repository;
 
 import com.springtest.study.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRespository extends JpaRepository<Todo, Long> {
 }
